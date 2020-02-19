f8344762c9a63b235aee1418a9ed68f5dcf54119
Jamie Ralph <jamesryanralph@outlook.com>
2020-02-19 10:52:28 +0000

    add error test for invalid dataset

---
+++ tests/testthat/test-get-data.R
@@ -0,0 +1,11 @@
+context("Valid dataset id returns dataframe")
+
+test_df <- get_dataset("hlth0515")
+
+test_that("get_dataset with valid id returns a dataframe", {
+          expect_true(is.data.frame(test_df))
+          })
+
+test_that("invalid id returns an error", {
+          expect_error(get_dataset("XXXXX"))
+          })
--- tests/testthat/test-get_data.R
+++
@@ -1,7 +0,0 @@
-context("Returns dataframe")
-
-test_df <- get_dataset("hlth0515")
-
-test_that("get_dataset returns a dataframe", {
-          expect_true(is.data.frame(test_df))
-          })
