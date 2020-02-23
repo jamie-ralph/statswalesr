74b1e41ddba4ba5653ab26f5ac997ac182bff7c4
Jamie Ralph <jamesryanralph@outlook.com>
2020-02-23 17:22:05 +0000

    add test for numeric input

--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -1,11 +1,15 @@
-context("Valid dataset id returns dataframe")
+context("Only a single valid id returns a dataframe")
 
 test_df <- statswalesr::get_dataset("hlth0515")
 
-test_that("get_dataset with valid id returns a dataframe", {
-          expect_true(is.data.frame(test_df))
-          })
+test_that("get_dataset with a valid id returns a dataframe", {
+  expect_true(is.data.frame(test_df))
+  })
 
 test_that("invalid id returns an error", {
-          expect_error(rstatswales::get_dataset("XXXXX"))
-          })
+  expect_error(statswalesr::get_dataset("XXXXX"))
+  })
+
+test_that("numeric input returns an error", {
+  expect_error(statswalesr::get_dataset(1234))
+  })
