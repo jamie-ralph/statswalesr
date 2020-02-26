ecc9715dad9f60a182fbf907c832cc3d5ec8e042
Jamie Ralph <jamesryanralph@outlook.com>
2020-02-26 16:30:36 +0000

    amend metadata function name

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,6 +1,6 @@
 #' Retrieve metadata of a dataset from StatsWales
 #'
-#' \code{get_metadata} returns a dataframe containing metadata from
+#' \code{statswales_get_metadata} returns a dataframe containing metadata from
 #' \href{https://statswales.gov.wales}{StatsWales} using a dataset id.
 #'
 #' @param id A dataset id as a string
@@ -8,10 +8,10 @@
 #'     the requested metadata in a dataframe. If the id is not
 #'     valid, the function will return an HTTP error.
 #' @examples
-#' metadata <- get_metadata("hlth0515")
+#' metadata <- statswales_get_metadata("hlth0515")
 #'
 #' @export
-get_metadata <- function(id) {
+statswales_get_metadata <- function(id) {
 
   # Define url containing metadata in JSON format
   url <- paste0("http://open.statswales.gov.wales/en-gb/discover/metadata?$filter=Dataset%20eq%20%27",
