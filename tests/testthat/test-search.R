17f5513cc01c33488647ae7996f8000425718af4
Jamie Ralph <jamesryanralph@outlook.com>
2020-03-19 20:54:42 +0000

    add test for non-character input

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -1,7 +1,11 @@
-context("Search function returns a dataframe")
+context("Valid search term returns a dataframe")
 
 test_search <- statswalesr::statswales_search("crop")
 
 test_that("valid search terms return a dataset", {
           expect_true(is.data.frame(test_search))
+          })
+
+test_that("Non-character input throws an error", {
+          expect_error(statswalesr::statswales_search(123))
           })
